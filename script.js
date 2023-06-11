33a8ec90f272814bb22fab2227007609658b4ff7
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-11 12:14:13 +0600

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
     setTimeout(
         function open(event){
+            document.getElementById("overlay").style.display = "block";
             document.querySelector(".popup").style.display = "block";
         },
         1000
@@ -10,7 +11,9 @@ window.addEventListener("load", function() {
 
 document.querySelector("#close").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 });
+
 
 // Transfer Input value to inner HTML
 function myFunction() {
